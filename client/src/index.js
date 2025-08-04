84512b60496b202ea7440632537e800ec522611c
Yana Ifraimov <yanai@Yanas-MacBook-Pro.local>
2025-08-04 16:08:26 +0300

    update

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./components/Dashboard";
 import reportWebVitals from "./reportWebVitals";
 
+//comment comment
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
