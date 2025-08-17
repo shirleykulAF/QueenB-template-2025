d0534b3857c403802453f90530ee245dbfd84e78
sapirs123 <sapirs350@gmail.com>
2025-08-17 12:57:57 +0300

    Added style to mentor home page

--- client/src/pages/MentorHome/MentorHome.js
+++ client/src/pages/MentorHome/MentorHome.js
@@ -3,6 +3,7 @@ import MentorInfo from "../../components/Mentor/MentorInfo/MentorInfo";
 import MentorContact from "../../components/Mentor/MentorContact/MentorContact";
 import { useMentorData } from "../../hooks/useMentorData";
 import { useEffect } from "react";
+import "./MentorHome.css";
 
 const MentorHome = ( {user} ) => {
   
@@ -17,15 +18,17 @@ const MentorHome = ( {user} ) => {
     }
   }, [mentor]);
   
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-  if (!mentor) return <div>No mentor data found</div>;
+  if (loading) return <div className="mentor-home-container loading">Loading...</div>;
+  if (error) return <div className="mentor-home-container error">Error: {error.message}</div>;
+  if (!mentor) return <div className="mentor-home-container error">No mentor data found</div>;
 
   return (
-    <div className="mentor-info">
-      <MentorHeader mentor={mentor} />
-      <MentorInfo mentor={mentor} />
-      <MentorContact mentor={mentor} />
+    <div className="mentor-home-container">
+      <div className="mentor-info">
+        <MentorHeader mentor={mentor} />
+        <MentorInfo mentor={mentor} />
+        <MentorContact mentor={mentor} />
+      </div>
     </div>
   );
 }
